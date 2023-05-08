ed276c65116b926cf92590c49a11d8f6f026cf8f
DTuniverse <127115560+DTuniverse@users.noreply.github.com>
2023-05-08 16:10:31 +0200

    implemented fetching

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [memes, setMemes] = useState([]);
+
+  const fetchMemes = async () => {
+    try {
+      const res = await fetch("https://api.imgflip.com/get_memes");
+      const data = await res.json();
+      setMemes(data.data.memes);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    fetchMemes();
+  }, []);
+  console.log(memes);
+  return (
+    <div className="App">
+      <h1>Memes Generator</h1>
+      <img src={memes[0]?.url} alt={memes[0]?.name} />
+    </div>
+  );
 }
 
 export default App;
